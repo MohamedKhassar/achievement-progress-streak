9367eaffb9bff7f9f192f6f304b6928780a90c85
Mohamed Khassar <mohamedkhassar775@gmail.com>
2026-03-09 00:33:16 +0000

    fix: correct casing of the app.css import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import './app.css'
+import './App.css'
 import CircleProgressbar from './components/CircleProgressbar'
 import StreakCircleProgressbar from './components/StreakCircleProgressbar'
 import axios from 'axios'
